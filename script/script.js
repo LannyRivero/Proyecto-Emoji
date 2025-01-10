39471b423a69b7669fdc9f40b3eed0523e3c7d99
LannyRivero <lannyrivero288@gmail.com>
2025-01-10 17:37:30 +0100

    commit

--- script/script.js
+++ script/script.js
@@ -1,25 +1,23 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.card');
-    let lastClickedCard = null; // Para rastrear la última tarjeta clicada
+    let lastClickedCard = null; 
 
-    // Agregar evento de clic a todas las tarjetas
+    
     cards.forEach((card) => {
         card.addEventListener('click', () => {
-            if (lastClickedCard) {
+            if (lastClickedCard) {              
                 
-                // Hacer reaparecer la tarjeta previamente oculta
                 lastClickedCard.style.visibility = 'visible';
                 lastClickedCard.style.opacity = '1';
             }
 
-            // Si la tarjeta clicada no está oculta, ocultarla
             if (card.style.visibility !== 'hidden') {
                 card.style.visibility = 'hidden';
                 card.style.opacity = '0';
             }
 
-            // Guardar la tarjeta actual como la última clicada
+            
             lastClickedCard = card;
         });
     });      
